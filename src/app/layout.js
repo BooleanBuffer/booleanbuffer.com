f794a29caf76c852f724cf673c06ce4e7ad64f10
Nitin Jayant <nitin@Nitins-MacBook-Air.local>
2024-11-02 20:37:28 +0530

    first

---
+++ src/app/layout.js
@@ -0,0 +1,14 @@
+import "./globals.css";
+
+export const metadata = {
+    title: "BooleanBuffer",
+    description: "BooleanBuffer",
+};
+
+export default function RootLayout({ children }) {
+    return (
+        <html lang="en">
+            <body>{children}</body>
+        </html>
+    );
+}
